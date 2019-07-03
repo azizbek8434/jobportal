e680d0bee28fb994479e51753f6b95f607555658
azizbek8434 <azizbek_muzaffarov@mail.ru>
2019-07-03 22:01:01 +0500

    implemented factory & seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,7 +1,9 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
+use App\Job;
 use App\User;
+use App\Company;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 
@@ -20,8 +22,42 @@ $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
+        'user_type'=> 'seeker',
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+    ];
+});
+
+
+$factory->define(Company::class, function (Faker $faker) {
+    return [
+        'user_id' => User::all()->random()->id,
+        'cname' => $name = $faker->company,
+        'slug' => str_slug($name),
+        'address' => $faker->address,
+        'phone' => $faker->phoneNumber,
+        'website' => $faker->domainName,
+        'logo' => 'avatars/man.jpg',        
+        'cover_photo' => 'cover/banner.png',
+        'slogan' => 'Learn earn and grow',
+        'description' => $faker->paragraph(rand(2,10))
+    ];
+});
+
+
+$factory->define(Job::class, function (Faker $faker) {
+    return [
+        'user_id' => User::all()->random()->id,
+        'company_id' => Company::all()->random()->id,
+        'title' => $title = $faker->text,
+        'slug' => str_slug($title),
+        'description' => $faker->paragraph(rand(2,10)),
+        'position' => $faker->jobTitle,
+        'address' => $faker->address,
+        'category_id' => rand(1,5),
+        'type' => 'Fulltime',
+        'status' => rand(0, 1),
+        'last_date' => $faker->DateTime
     ];
 });
